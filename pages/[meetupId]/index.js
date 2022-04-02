e6c53f0f347de9e3e2bb914fd9c3137d15f07b84
amirho3einb <amirhossinbalagar@gmail.com>
2022-04-02 13:22:41 +0430

    add detail page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetailPage = () => {
+  return (
+    <MeetupDetail
+      image="https://kite.ir/UploadData/Packages/%D8%A2%D9%86%D8%AA%D8%A7%D9%84%DB%8C%D8%A7.jpg_f0jqvr3a.4sc.jpg"
+      title="A first Meetup"
+      address="Some address 5, 12345 some city"
+      description="this is first meetup"
+    />
+  );
+};
+
+export default MeetupDetailPage;
